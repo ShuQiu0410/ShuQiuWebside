2811eb4a15e78304dee6af0bc66fbda43d918e76
ShuQiu0410 <a931019a@gmail.com>
2023-06-11 07:34:07 +0800

    20230611
    tools/rollChance

---
+++ JavaScript/rollChance.js
@@ -0,0 +1,15 @@
+var times = document.getElementById("times");
+var chance = document.getElementById("chance");
+var button = document.getElementById("button");
+
+button.addEventListener("click",function(){
+    if(chance.value>100){
+        document.getElementById("ans").innerHTML="Input Error";
+    }else if(chance<0){
+        document.getElementById("ans").innerHTML="Input Error";
+    }else{
+        var ans = (1-(Math.pow((1-chance.value/100),times.value)))*100;
+        ans = Math.round(ans*10000)/10000;
+        document.getElementById("ans").innerHTML=ans+"%";
+    }
+})
